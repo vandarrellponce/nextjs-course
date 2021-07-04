f22318e469f1ada4c2b3eb1b501d4d525c2efc57
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-04 09:35:35 +0800

    extracting dynamic route data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router'
+
 // our-domain.com/news/[newsId]
 
 const DetailPage = () => {
-	return <h1>The Detail Page</h1>
+	const newsId = useRouter().query.newsId
+
+	// send a request to the backend API
+	// to fetch the news item with newsId
+
+	return <h1>The Detail Page </h1>
 }
 
 export default DetailPage
