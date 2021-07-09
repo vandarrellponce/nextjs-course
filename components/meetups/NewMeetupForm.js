43bcac93e48c27514646839d59d2087a48b10208
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-09 15:40:30 +0800

    adding a form for adding meetups

--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import { useRef } from 'react'
 import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
   const titleInputRef = useRef()
   const imageInputRef = useRef()
   const addressInputRef = useRef()
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,17 @@
 // our-domain.com/new-meetup
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+import Layout from '../../components/layout/Layout'
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (data) => {
+    console.log(data)
+  }
+  return (
+    <Layout>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Layout>
+  )
+}
+
+export default NewMeetupPage
