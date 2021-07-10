a610d1f1eb940a439e6b7d24942e188a6554fe4b
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-10 16:32:23 +0800

    working with dynamic path params in getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,29 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail'
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-mount-fuji-japan.jpg"
-      title="a title"
-      address="this is an address"
-      description="descriptionssss"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   )
+}
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-mount-fuji-japan.jpg',
+        title: 'a title',
+        address: 'this is an address',
+        description: 'descriptionssss',
+      },
+    },
+  }
 }
 
 export default MeetupDetails
