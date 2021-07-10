d9e70a561271b15df3a7a951bf7280becf155070
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-10 14:12:09 +0800

    adding custom component and styling with css

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
 
-const MeetupDetail = (props) => {
-  return <div>hey</div>
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-mount-fuji-japan.jpg"
+      title="a title"
+      address="this is an address"
+      description="descriptionssss"
+    />
+  )
 }
 
-export default MeetupDetail
+export default MeetupDetails
