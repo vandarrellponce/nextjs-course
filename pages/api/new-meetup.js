94b570bf1f542a1762d7de7ceacea838c2cc2b71
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-11 12:57:32 +0800

    sending http request to api routes & fixing mongodb uri

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,13 +6,17 @@ const handler = async (req, res) => {
   if (req.method == 'POST') {
     try {
       const client = await MongoClient.connect(
-        'MONGO_URI = mongodb+srv://darrell:admin12345@cluster0.reoms.mongodb.net/react-meetup?retryWrites=true&w=majority'
+        'mongodb+srv://darrell:admin12345@cluster0.reoms.mongodb.net/react-meetup?retryWrites=true&w=majority',
+        { useUnifiedTopology: true }
       )
       // adding item to collection
       const db = client.db()
+
       const meetupCollection = db.collection('meetups')
-      const result = await meetupCollection.insertOne(request.body)
-      console.log(result)
+      const result = await meetupCollection.insertOne(req.body)
+
+      console.log(result.insertedId)
+
       client.close()
 
       res.status(201).json({ message: 'inserted' })
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,21 @@
 // our-domain.com/new-meetup
 
+import { useRouter } from 'next/router'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (data) => {
-    console.log(data)
+  const router = useRouter()
+  const addMeetupHandler = async (data) => {
+    const res = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const result = await res.json()
+    console.log(result.message)
+    router.push('/')
   }
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
 }
