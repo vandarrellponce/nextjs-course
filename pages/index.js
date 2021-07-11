4e8abfc86713c07a5781d3fc8e2c206d167f748b
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-11 18:58:07 +0800

    pre rendering meetups data

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,6 @@
 import MeetupList from '../components/meetups/MeetupList'
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image:
-      'https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-mount-fuji-japan.jpg',
-    address: 'some address',
-    description: 'this is a first meetup',
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image:
-      'https://img.traveltriangle.com/blog/wp-content/uploads/2020/02/city-skyline-of-Sydney-Australia.jpg',
-    address: 'some address',
-    description: 'this is the second meetup meetup',
-  },
-]
+import { MongoClient } from 'mongodb'
 
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />
@@ -26,11 +9,36 @@ const HomePage = (props) => {
 export const getStaticProps = async () => {
   // fetch data from api or database
 
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-    revalidate: 1,
+  // get data directly from database
+  try {
+    const client = await MongoClient.connect(
+      'mongodb+srv://darrell:admin12345@cluster0.reoms.mongodb.net/react-meetup?retryWrites=true&w=majority',
+      { useUnifiedTopology: true }
+    )
+    const db = client.db()
+    const meetupCollections = db.collection('meetups')
+    const meetups = await meetupCollections.find({}).toArray()
+
+    if (!meetups) throw new Error('something went wrong')
+
+    client.close()
+
+    return {
+      props: {
+        meetups: meetups.map((meetup) => {
+          return {
+            title: meetup.title,
+            image: meetup.image,
+            address: meetup.address,
+            description: meetup.description,
+            id: meetup._id.toString(),
+          }
+        }),
+      },
+      revalidate: 1,
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 }
 
