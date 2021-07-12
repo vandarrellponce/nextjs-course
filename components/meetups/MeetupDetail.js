ecbfdda5fd70312f3d5f385eaf5fb61c13aac784
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-12 16:24:22 +0800

    some corrections

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
 import classes from './MeetupDetail.module.css'
+import Image from 'next/image'
 
 const MeetupDetail = (props) => {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt="place image" />
+      <Image src={props.image} alt="place image" />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
