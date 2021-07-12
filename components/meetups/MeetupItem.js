41c3dc76405bb37b9173002c7abeafb8b52dd2e4
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-12 16:09:45 +0800

    some corrections

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,6 +1,7 @@
 import Card from '../ui/Card'
 import classes from './MeetupItem.module.css'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 function MeetupItem(props) {
   const router = useRouter()
@@ -13,7 +14,7 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
